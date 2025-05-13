1265de62ea7dde31507d13d024c530fc153a23e1
aleberlo27 <alejandrabernallopez1@gmial.com>
2025-05-13 21:53:50 +0200

    13/05/2025

--- src/app/pages/application/pages/initial/initial.component.ts
+++ src/app/pages/application/pages/initial/initial.component.ts
@@ -1,18 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { IonContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonCard, IonButton } from '@ionic/angular/standalone';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { IonContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonCard, IonButton, IonList, IonItem, IonToggle, IonPopover } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-initial',
   standalone: true,
-  imports: [IonCardSubtitle, IonCardHeader, IonContent, IonCardTitle, IonCardContent, IonCard, IonButton, FormsModule],
+  imports: [IonPopover, IonPopover, IonCardSubtitle, IonCardHeader, IonContent, IonCardTitle, IonCardContent, IonCard, IonButton, FormsModule],
   templateUrl: './initial.component.html',
-  styleUrl:'./initial.component.css',
+  styleUrl: './initial.component.css',
 })
 export class InitialComponent {
+  paletteToggle = false;
   user: any;
 
+  @ViewChild('popover') popover!: HTMLIonPopoverElement;
+
+  isOpen = false;
+
+  presentPopover(e: Event) {
+    this.popover.event = e;
+    this.isOpen = true;
+  }
+
   ngOnInit() {
+    /* this.initDarkMode(); */
     const userData = localStorage.getItem('user');
     if (userData) {
       this.user = JSON.parse(userData);
@@ -25,4 +36,31 @@ export class InitialComponent {
     console.log('Turno iniciado');
     // Aquí podrías llamar a un servicio que registre el inicio del turno
   }
+
+  /* DARK MODE INTENTO */
+/*
+  toggleChange(event: any) {
+    this.toggleDarkPalette(event.detail.checked);
+  }
+
+  toggleDarkPalette(shouldAdd: boolean) {
+    document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
+  }
+
+  initDarkMode() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Initialize the dark palette based on the initial
+    // value of the prefers-color-scheme media query
+    this.initializeDarkPalette(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkPalette(mediaQuery.matches));
+  }
+
+  initializeDarkPalette(isDark: boolean) {
+    this.paletteToggle = isDark;
+    this.toggleDarkPalette(isDark);
+  } */
 }
+
